a7a3a44a300c65df26be634e6359ab76bff34924
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-09-11 15:53:18 -0600

    frontend: login

--- apps/client/src/pages/Login.jsx
+++ apps/client/src/pages/Login.jsx
@@ -4,7 +4,9 @@ import React from "react";
 function Login() {
 	return (
 		<div className="flex h-full items-center justify-center min-h-screen">
-			<Form className="w-1/3 -mt-32">Hola </Form>
+			<Form className="w-1/3 -mt-32 bg-purple-300">
+				<h2 className="text-3xl">Inicia sesión para jugar</h2>
+			</Form>
 		</div>
 	);
 }
--- apps/server/src/routes/health.route.ts
+++ apps/server/src/routes/health.route.ts
@@ -1,6 +1,6 @@
-import {Express} from 'express';
-import {ContainerBuilder} from "node-dependency-injection";
-import {HealthController} from "../shared/infrastructure/controllers/HealthController";
+import { Express } from 'express';
+import { ContainerBuilder } from "node-dependency-injection";
+import { HealthController } from "../shared/infrastructure/controllers/HealthController";
 
 export const register = async (app: Express, container: ContainerBuilder) => {
     const healthController = container.get<HealthController>('HealthController');
