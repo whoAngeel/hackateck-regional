d2d6a14effd1a5f5b5e71de2f1a82170fae23663
Ronaldo Acevedo Ojeda <ronaldoa.ojeda@gmail.com>
2024-09-11 19:51:56 -0600

    Mundo 3D

---
+++ apps/client/src/constants/icon.js
@@ -0,0 +1,11 @@
+import cronometro from "../assets/icons/cronometro.png";
+import temperatura from "../assets/icons/la-temperatura.png";
+import latido from "../assets/icons/latido-del-corazon.png";
+import plantado from "../assets/icons/plantando.png";
+
+export default{
+    cronometro,
+    temperatura,
+    latido,
+    plantado
+}
--- apps/client/src/pages/game/Game.jsx
+++ apps/client/src/pages/game/Game.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Suspense, useState } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import Earth from "../../../public/Earth";
+import EarthCartoon from "../../../public/earthCarton/EarthCartoon";
+import Pony from "../../../public/lowpony/Pony"
+import { Environment, OrbitControls } from "@react-three/drei";
+import Stat from "./../../components/Stat";
+import icon from "./../../constants/icon";
 
 function Game() {
-	return <div>Game</div>;
+	return (
+		<div className="flex flex-row home" id="home">
+      <div className=" w-[10%] text-center rounded-lg">
+        <div className="bg-[#008777] p-2">
+          <p className="text-white font-plight">Estadisticas</p>
+        </div>
+
+        <Stat name="Dias" icon={icon.latido} cifra="2" />
+          <Stat name="Dias" icon={icon.plantado} cifra="2" />
+      </div>
+      <div className="w-[80%]">
+        <Canvas camera={{ position: [0, 0, 2.5] }}>
+          <ambientLight />
+          <OrbitControls enableZoom={false} />
+
+          <Suspense fallback={null}>
+            <EarthCartoon />
+          </Suspense>
+          <Environment preset="sunset" />
+        </Canvas>
+      </div>
+      <div>
+        <p className="text-white font-mono">Dias: </p>
+        <p className="text-white font-mono">Esperanza de vida: </p>
+        <p className="text-white font-mono ">CO2: </p>
+        <p className="text-white font-mono">Nivel de mar: </p>
+        <p className="text-white font-mono">Temperatura: </p>
+      </div>
+    </div>
+	);
 }
 
 export default Game;
