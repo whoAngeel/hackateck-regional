423468c9ee6220df8e2dfa50d4fc9ee7edbd0d82
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-09-11 20:29:43 -0600

    cambios en front

--- apps/client/src/components/ui/StatCard.jsx
+++ apps/client/src/components/ui/StatCard.jsx
@@ -4,7 +4,7 @@ import icon from "../../constants/icon";
 
 function StatCard() {
 	return (
-		<div className="fixed top-0 left-0 flex flex-col w-1/12 sm:w-1/12 md:w-2/12 h-20 justify-center items-center bg-primary-500">
+		<div className="fixed top-0 left-0 flex flex-col w-3/12 sm:w-1/12 md:w-2/12 h-20 justify-center items-center bg-primary-500">
 			<p className="text-white font-semibold text-center">Estadisticas</p>
 			<div className="w-full flex justify-center flex-col items-center">
 				<Stat name="Dias" icon={icon.latido} cifra="2" />
