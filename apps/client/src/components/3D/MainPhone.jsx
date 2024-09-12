22cfd3cd44cc3b00a479033b3b8e489fa6e28210
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2024-09-12 10:50:37 -0600

    invite friend button

--- apps/client/src/components/3D/MainPhone.jsx
+++ apps/client/src/components/3D/MainPhone.jsx
@@ -13,6 +13,9 @@ import StatCard from "../../components/ui/StatCard";
 import { Sprite, SpriteMaterial, TextureLoader } from "three";
 import Board from "../../../public/board/Board";
 import Arrow from "../../../public/arrow/Arrow";
+import {Avatar} from "antd";
+import {useSelector} from "react-redux";
+import InviteFriendButton from "../ui/InviteFriendButton.jsx";
 
 const ResponsiveCamera = () => {
   const { camera, size } = useThree();
@@ -33,93 +36,108 @@ const ResponsiveCamera = () => {
 };
 
 function MainPhone() {
+  const {user} = useSelector((state) => state.auth);
   return (
-    <div className="flex flex-row md:flex-col home  bg-black" id="home">
-      <div className="w-full mx-auto md:mt-[-150px]">
-        <Canvas camera={{ position: [0, 0, 2.5] }}>
-          <ResponsiveCamera />
-          <ambientLight />
-          <OrbitControls enableZoom={false} />
+      <div className="flex flex-row md:flex-col home min-h-screen bg-black" id="home">
+        <div
+            className="p-4 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 flex-col flex gap-4 m-4 justify-center absolute right-0 left-0 bottom-0">
+          <h2 className="text-gray-300 font-bold">Guardianes de este planeta</h2>
+          <div className="flex gap-4 items-center w-full">
+            <Avatar size="small" className="cursor-pointer bg-green-300 min-w-fit">
+              {user.fullName[0]}
+            </Avatar>
+            <Avatar size="small" className="cursor-pointer bg-rose-300">
+              {user.fullName[1].toUpperCase()}
+            </Avatar>
+            <InviteFriendButton/>
+          </div>
+        </div>
+        <div className="w-full min-h-screen mx-auto md:mt-[-150px]">
+          <Canvas camera={{position: [0, 0, 2.5]}}>
+            <ResponsiveCamera/>
+            <ambientLight/>
+            <OrbitControls enableZoom={false}/>
 
-          <Suspense fallback={null}>
-            <mesh scale={[0.002, 0.002, 0.002]} position={[-.5, -2.8, 0.5]}>
-              <Board />
-            </mesh>
-            <mesh position={[0, -1.7, 0.5]}>
-              <boxGeometry args={[0.82, 1, 0.01]} />
-              <meshStandardMaterial color={"#38ad30"} />
-              {/* Texto dentro de la malla */}
+            <Suspense fallback={null}>
+              <mesh scale={[0.002, 0.002, 0.002]} position={[-.5, -2.8, 0.5]}>
+                <Board/>
+              </mesh>
+              <mesh position={[0, -1.7, 0.5]}>
+                <boxGeometry args={[0.82, 1, 0.01]}/>
+                <meshStandardMaterial color={"#38ad30"}/>
+                {/* Texto dentro de la malla */}
 
-              <Text
-                position={[0, 0.4, 0.03]} // Ajusta la posición del texto en la malla
-                fontSize={0.07} // Tamaño del texto
-                color="#ffffff" // Color del texto
-                anchorX="center" // Alineación horizontal
-                anchorY="middle" // Alineación vertical
-                fontWeight="bold"
-              >
-                Estadisticas
-              </Text>
+                <Text
+                    position={[0, 0.4, 0.03]} // Ajusta la posición del texto en la malla
+                    fontSize={0.07} // Tamaño del texto
+                    color="#ffffff" // Color del texto
+                    anchorX="center" // Alineación horizontal
+                    anchorY="middle" // Alineación vertical
+                    fontWeight="bold"
+                >
+                  Estadisticas
+                </Text>
 
-              <Text
-                position={[0, 0.25, 0.03]} // Ajusta la posición del texto en la malla
-                fontSize={0.06} // Tamaño del texto
-                color="#ffffff" // Color del texto
-                anchorX="center" // Alineación horizontal
-                anchorY="middle" // Alineación vertical
-                fontWeight="bold"
-              >
-                Dias: 2
-              </Text>
-              <Text
-                position={[0, 0.15, 0.03]} // Ajusta la posición del texto en la malla
-                fontSize={0.06} // Tamaño del texto
-                color="#ffffff" // Color del texto
-                anchorX="center" // Alineación horizontal
-                anchorY="middle" // Alineación vertical
-                fontWeight="bold"
-              >
-                Co2: 2
-              </Text>
-              <Text
-                position={[0, 0.05, 0.03]} // Ajusta la posición del texto en la malla
-                fontSize={0.06} // Tamaño del texto
-                color="#ffffff" // Color del texto
-                anchorX="center" // Alineación horizontal
-                anchorY="middle" // Alineación vertical
-                fontWeight="bold"
-              >
-                Esperanza de Vida: 2
-              </Text>
-              <Text
-                position={[0, -0.05, 0.03]} // Ajusta la posición del texto en la malla
-                fontSize={0.06} // Tamaño del texto
-                color="#ffffff" // Color del texto
-                anchorX="center" // Alineación horizontal
-                anchorY="middle" // Alineación vertical
-                fontWeight="bold"
-              >
-                Temperatura: 2C
-              </Text>
-            </mesh>
-            
-            <mesh position={[0,0.2,0]}>
-              <EarthCartoon />
-            </mesh>
-          </Suspense>
-          <Stars
-            radius={200}
-            depth={100}
-            count={500}
-            factor={20}
-            saturation={0}
-            fade
-            speed={1}
-          />
-          <Environment preset="city" />
-        </Canvas>
+                <Text
+                    position={[0, 0.25, 0.03]} // Ajusta la posición del texto en la malla
+                    fontSize={0.06} // Tamaño del texto
+                    color="#ffffff" // Color del texto
+                    anchorX="center" // Alineación horizontal
+                    anchorY="middle" // Alineación vertical
+                    fontWeight="bold"
+                >
+                  Dias: 2
+                </Text>
+                <Text
+                    position={[0, 0.15, 0.03]} // Ajusta la posición del texto en la malla
+                    fontSize={0.06} // Tamaño del texto
+                    color="#ffffff" // Color del texto
+                    anchorX="center" // Alineación horizontal
+                    anchorY="middle" // Alineación vertical
+                    fontWeight="bold"
+                >
+                  Co2: 2
+                </Text>
+                <Text
+                    position={[0, 0.05, 0.03]} // Ajusta la posición del texto en la malla
+                    fontSize={0.06} // Tamaño del texto
+                    color="#ffffff" // Color del texto
+                    anchorX="center" // Alineación horizontal
+                    anchorY="middle" // Alineación vertical
+                    fontWeight="bold"
+                >
+                  Esperanza de Vida: 2
+                </Text>
+                <Text
+                    position={[0, -0.05, 0.03]} // Ajusta la posición del texto en la malla
+                    fontSize={0.06} // Tamaño del texto
+                    color="#ffffff" // Color del texto
+                    anchorX="center" // Alineación horizontal
+                    anchorY="middle" // Alineación vertical
+                    fontWeight="bold"
+                >
+                  Temperatura: 2C
+                </Text>
+              </mesh>
+
+              <mesh position={[0, 0.2, 0]}>
+                <EarthCartoon/>
+              </mesh>
+            </Suspense>
+            <Stars
+                radius={200}
+                depth={100}
+                count={500}
+                factor={20}
+                saturation={0}
+                fade
+                speed={1}
+            />
+            <Environment preset="city"/>
+          </Canvas>
+        </div>
       </div>
-    </div>
   );
 }
+
 export default MainPhone;
--- apps/client/src/pages/game/Game.jsx
+++ apps/client/src/pages/game/Game.jsx
@@ -1,23 +1,26 @@
-import { useMediaQuery } from "react-responsive";
+import {useMediaQuery} from "react-responsive";
 import MainPC from "../../components/3D/MainPC";
 import MainPhone from "../../components/3D/MainPhone";
 import "./Game.css";
 
 
 function Game() {
-	const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
-  const isPC = useMediaQuery({ query: '(min-width: 768px)' });
-  return (
-	  <>
-      <div className="absolute bottom-0 mx-auto w-full text-center">
-        <h1 className="text-white font-psemibold text-3xl">Hackatec 2024</h1>
-        <h5 className="text-white font-plight md:text-sm">Pulsa sobre cualquier avion</h5>
-      </div>
-      {isMobile && <MainPhone/>} {/* Carga componente para móviles */}
-      {isPC && <MainPC/>} {/* Carga componente para PCs */}
-    </>
-	
-  );
+    const isMobile = useMediaQuery({query: '(max-width: 767px)'});
+    const isPC = useMediaQuery({query: '(min-width: 768px)'});
+
+
+
+    return (
+        <>
+            <div className="absolute bottom-0 mx-auto w-full text-center sm:hidden">
+                <h1 className="text-white font-psemibold text-3xl ">Hackatec 2024</h1>
+                <h5 className="text-white font-plight md:text-sm">Pulsa sobre cualquier avion</h5>
+            </div>
+            {isMobile && <MainPhone/>} {/* Carga componente para móviles */}
+            {isPC && <MainPC/>} {/* Carga componente para PCs */}
+        </>
+
+    );
 }
 
 export default Game;
